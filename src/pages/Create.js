16b432bf3a18da1bee134590b0fb3c98f3573e5b
khanhduy1407 <kn145660@gmail.com>
2022-02-01 20:40:57 +0700

    makeStyles Hook (Custom CSS)

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,16 +1,31 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
-import ButtonGroup from '@material-ui/core/ButtonGroup'
 import Container from '@material-ui/core/Container'
-import AcUnitOutlinedIcon from '@material-ui/icons/AcUnitOutlined';
-import SendIcon from '@material-ui/icons/Send';
-import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 60,
+    backgroundColor: 'violet',
+    '&:hover': {
+      backgroundColor: 'blue'
+    }
+  },
+  title: {
+    textDecoration: 'underline',
+    marginBottom: 20
+  }
+})
 
 export default function Create() {
+  const classes = useStyles()
+
   return (
     <Container>
       <Typography
+        className={classes.title}
         variant="h6"
         color="textSecondary"
         component="h2"
@@ -19,6 +34,7 @@ export default function Create() {
       </Typography>
 
       <Button
+        className={classes.btn}
         onClick={() => console.log("you click me")}
         type="submit"
         color="secondary"
@@ -26,15 +42,6 @@ export default function Create() {
         endIcon={<KeyboardArrowRightIcon />}>
         Submit
       </Button>
-
-      {/* icons */}
-      {/* <br />
-      <AcUnitOutlinedIcon />
-      <AcUnitOutlinedIcon color="secondary" fontSize="large" />
-      <AcUnitOutlinedIcon color="secondary" fontSize="small" />
-      <AcUnitOutlinedIcon color="action" fontSize="small" />
-      <AcUnitOutlinedIcon color="error" fontSize="small" />
-      <AcUnitOutlinedIcon color="disabled" fontSize="small" /> */}
     </Container>
   )
 }
