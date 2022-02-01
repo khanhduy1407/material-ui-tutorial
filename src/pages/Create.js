c04fd90a8596b3e48dba683736a86843ebe69564
khanhduy1407 <kn145660@gmail.com>
2022-02-01 20:11:38 +0700

    Typography

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import Typography from '@material-ui/core/Typography'
 
 export default function Create() {
   return (
     <div>
-      Create page
+      {/* <Typography
+        variant="h1"
+        color="primary"
+        align="center">
+        Create a new site
+      </Typography> */}
+      {/* <Typography
+        noWrap
+        color="secondary">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non, nesciunt? Suscipit, voluptas minima rem molestiae fugit inventore pariatur iure ullam voluptates repellat fugiat, sint aut. Enim earum aut sapiente odit necessitatibus.
+      </Typography> */}
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom>
+        Create a new site
+      </Typography>
     </div>
   )
 }
