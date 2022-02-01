2a95f0fe084f38bc41dcf31c612b71550b603ac0
khanhduy1407 <kn145660@gmail.com>
2022-02-01 20:34:02 +0700

    Icons

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -3,6 +3,9 @@ import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import ButtonGroup from '@material-ui/core/ButtonGroup'
 import Container from '@material-ui/core/Container'
+import AcUnitOutlinedIcon from '@material-ui/icons/AcUnitOutlined';
+import SendIcon from '@material-ui/icons/Send';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 
 export default function Create() {
   return (
@@ -19,18 +22,19 @@ export default function Create() {
         onClick={() => console.log("you click me")}
         type="submit"
         color="secondary"
-        variant="contained">
+        variant="contained"
+        endIcon={<KeyboardArrowRightIcon />}>
         Submit
       </Button>
 
-      {/* <Button type="submit">Submit</Button>
-      <Button type="submit" color="secondary" variant="outlined">Submit</Button>
-
-      <ButtonGroup color="secondary" variant="contained">
-        <Button>One</Button>
-        <Button>Two</Button>
-        <Button>Three</Button>
-      </ButtonGroup> */}
+      {/* icons */}
+      {/* <br />
+      <AcUnitOutlinedIcon />
+      <AcUnitOutlinedIcon color="secondary" fontSize="large" />
+      <AcUnitOutlinedIcon color="secondary" fontSize="small" />
+      <AcUnitOutlinedIcon color="action" fontSize="small" />
+      <AcUnitOutlinedIcon color="error" fontSize="small" />
+      <AcUnitOutlinedIcon color="disabled" fontSize="small" /> */}
     </Container>
   )
 }
